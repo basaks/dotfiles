499334b9023a972cd4b502322711f6cccdda6e51
Sudipta Basak <basaks@gmail.com>
2018-01-30 21:46:57 +1100

    also install gvim

--- centos_vim8_install.sh
+++ centos_vim8_install.sh
@@ -2,11 +2,11 @@
 
 # optional add on script to update vim
 # install vim8 for better python virtualenv support via command line
-sudo yum update -y
-sudo yum install -y ncurses-devel
+sudo apt update -y
+sudo apt install -y ncurses-devel
 wget https://github.com/vim/vim/archive/v8.0.1438.tar.gz && \
     tar -xzf v8.0.1438.tar.gz && \
     cd vim-8.0.1438/src/ && \
-    ./configure --enable-pythoninterp=yes --enable-python3interp=yes && \
+    ./configure --enable-pythoninterp=yes --enable-python3interp=yes --enable-gui && \
     make && \
     sudo make install
