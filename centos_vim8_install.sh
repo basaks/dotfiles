16da748d1f0c1f01b0e12c3407e75b48ec487541
Sudipta Basak <basaks@gmail.com>
2018-02-05 14:14:11 +1100

    need x11 build-deps for gvim

--- centos_vim8_install.sh
+++ centos_vim8_install.sh
@@ -4,6 +4,7 @@
 # install vim8 for better python virtualenv support via command line
 sudo yum update -y
 sudo yum install -y ncurses-devel
+sudo yum install -y yum-builddep vim-X11
 wget https://github.com/vim/vim/archive/v8.0.1438.tar.gz && \
     tar -xzf v8.0.1438.tar.gz && \
     cd vim-8.0.1438/src/ && \
