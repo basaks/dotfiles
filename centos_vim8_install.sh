026e90b1a5a246b56c7d915c4e3b3236caf28223
Sudipta Basak <basaks@gmail.com>
2018-03-28 05:57:42 +1100

    without sudo option

--- centos_vim8_install.sh
+++ centos_vim8_install.sh
@@ -5,12 +5,15 @@
 sudo yum update -y
 sudo yum install -y ncurses-devel
 sudo yum install -y yum-builddep vim-X11
+
+# without sudo use: ./configure --enable-pythoninterp=yes --enable-python3interp=yes --enable-gui --prefix=~/.local/
 wget https://github.com/vim/vim/archive/v8.0.1438.tar.gz && \
     tar -xzf v8.0.1438.tar.gz && \
     cd vim-8.0.1438/src/ && \
     ./configure --enable-pythoninterp=yes --enable-python3interp=yes --enable-gui && \
     make && \
     sudo make install
+
 
 # I like monaco font
 mkdir -p $HOME/.fonts/
