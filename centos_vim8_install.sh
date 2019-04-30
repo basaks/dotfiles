61817f9fab1ddb2f3658da3f25cb0eeda35e4491
Sudipta Basak <xsbk2@re-jumpdev-01.c.wx-bq-poc.internal>
2019-04-30 06:08:49 +0000

    update to latest vim

--- centos_vim8_install.sh
+++ centos_vim8_install.sh
@@ -2,14 +2,14 @@
 
 # optional add on script to update vim
 # install vim8 for better python virtualenv support via command line
-sudo yum update -y
-sudo yum install -y ncurses-devel
-sudo yum install -y yum-builddep vim-X11
+# sudo yum update -y
+# sudo yum install -y ncurses-devel
+# sudo yum install -y yum-builddep vim-X11
 
 # without sudo use: ./configure --enable-pythoninterp=yes --enable-python3interp=yes --enable-gui --prefix=~/.local/
-wget https://github.com/vim/vim/archive/v8.0.1438.tar.gz && \
-    tar -xzf v8.0.1438.tar.gz && \
-    cd vim-8.0.1438/src/ && \
+wget https://github.com/vim/vim/archive/v8.1.1239.tar.gz && \
+    tar -xzf v8.1.1239.tar.gz && \
+    cd vim-8.1.1239/src/ && \
     ./configure --enable-pythoninterp=yes --enable-python3interp=yes --enable-gui && \
     make && \
     sudo make install
@@ -20,3 +20,6 @@ mkdir -p $HOME/.fonts/
 wget https://gist.github.com/epegzz/1634235/raw/4691e901750591f9cab0b4ae8b7c0731ebf28cce/Monaco_Linux-Powerline.ttf -P $HOME/.fonts/
 fc-cache -vf
 
+
+# if using putty use the following to change to a nicer color scheme
+# if [ "$TERM" = xterm ]; then TERM=xterm-256color; fi
